5b78fb4d594d41edfb741f431b3bc7e957e3f826
Syed Ahmed Ali <139225219+syedahmedali568@users.noreply.github.com>
2023-12-19 22:32:09 +0500

    ok

--- New folder/LAB 7/Code/Question05.cpp
+++ New folder/LAB 7/Code/Question05.cpp
@@ -6,3 +6,35 @@
 // a. between 1 and n
 // b. between n and 1
 
+
+#include <iostream>
+using namespace std;
+void printOddNumbersUpToN(int n) {
+    if (n >= 1) {
+        printOddNumbersUpToN(n - 1);
+        if (n % 2 != 0) {
+            cout << n << " ";
+        }
+    }
+}
+void printOddNumbersDownTo1(int n) {
+    if (n >= 1) {
+        if (n % 2 != 0) {
+            cout << n << " ";
+        }
+        printOddNumbersDownTo1(n - 1);
+    }
+}
+int main() {
+    int n;
+    do {
+        cout << "Enter a positive integer (n): ";
+        cin >> n;
+    } while (n <= 0);
+    cout << "a. Odd numbers between 1 and " << n << ": ";
+    printOddNumbersUpToN(n);
+    cout << "\nb. Odd numbers between " << n << " and 1: ";
+    printOddNumbersDownTo1(n);
+    cout << endl;
+    return 0;
+}
